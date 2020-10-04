4ef9908517957d2f50720f167f4a2928c3c4ba6d
20chan <2@0chan.dev>
2020-10-04 15:57:19 +0000

    calendar weekdays styles

--- client/app/src/components/calendar.tsx
+++ client/app/src/components/calendar.tsx
@@ -1,10 +1,21 @@
 import React, { useState, } from "react";
+import "./calendar.css";
+
+const weekdays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
 export const Calendar = () => {
     return (
         <div className="calendar">
             <p className="calendar-title">2020 08</p>
-
+            <div className="calendar-container">
+                {
+                    weekdays.map(wd =>
+                        <div className="weekday-wrapper">
+                            <label className="weekday">{wd}</label>
+                        </div>
+                    )
+                }
+            </div>
         </div>
     );
 };
