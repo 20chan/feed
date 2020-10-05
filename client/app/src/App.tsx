a47d06caf5539af1921e79f38354edfd84935156
20chan <2@0chan.dev>
2020-10-05 16:12:38 +0000

    remove unused imports

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import LoginPage from "./login";
 import DashboardPage from "./dashboard";
 
