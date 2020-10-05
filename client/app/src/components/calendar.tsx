4875557d0b0b24e43ba7cff9d322acb27a82a218
20chan <2@0chan.dev>
2020-10-05 14:40:34 +0000

    implement calendar

--- client/app/src/components/calendar.tsx
+++ client/app/src/components/calendar.tsx
@@ -1,18 +1,42 @@
 import React, { useState, } from "react";
+import moment from "moment";
 import "./calendar.css";
 
 const weekdays = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
 export const Calendar = () => {
+    const [date, setDate] = useState(moment());
+
+    const firstDayOfMonth = () => {
+        return Number(date.startOf("month").format("d"));
+    };
+    const daysInMonth = () => {
+        return date.daysInMonth();
+    };
+
+    const range = (length: number) => {
+        return Array.from({length}, (v, k) => k);
+    };
+
     return (
         <div className="calendar">
-            <p className="calendar-title">2020 08</p>
+            <p className="calendar-title">{date.format("YYYY MM")}</p>
             <div className="calendar-container">
                 {
                     weekdays.map(wd =>
-                        <div className="weekday-wrapper">
+                        <div className="cell weekday-wrapper" key={wd}>
                             <label className="weekday">{wd}</label>
                         </div>
+                    )
+                }
+                {
+                    range(firstDayOfMonth()).map(i =>
+                        <div className="cell day empty-day" key={i}></div>
+                    )
+                }
+                {
+                    range(daysInMonth()).map(day =>
+                        <div className="cell day" key={day}>{day + 1}</div>
                     )
                 }
             </div>
