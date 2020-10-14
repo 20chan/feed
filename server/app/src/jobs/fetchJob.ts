a3d248fc59c2d0e520ae29a853d5d0cd63ce740a
20chan <2@0chan.dev>
2020-10-14 12:40:28 +0900

    fetch job with intervals

---
+++ server/app/src/jobs/fetchJob.ts
@@ -0,0 +1,24 @@
+import { fetchChannel } from "../fetchFeeds";
+import { getAllSubscribes } from "../subscribes";
+
+export class FetchJob {
+    public interval: number;
+    public last: Date;
+    private i: NodeJS.Timeout;
+
+    constructor(interval: number) {
+        this.setInterval(interval);
+    }
+
+    public setInterval = (interval: number) => {
+        this.interval = interval;
+        clearInterval(this.i);
+        this.i = setInterval(async () => await this.check(), interval);
+    }
+
+    public check = async () => {
+        const subscribes = await getAllSubscribes();
+        await Promise.all(subscribes.map(async subs => await fetchChannel(subs)));
+        this.last = new Date();
+    }
+}
--- server/app/src/routes/feeds/fetch.ts
+++ server/app/src/routes/feeds/fetch.ts
@@ -1,10 +1,27 @@
 import * as express from "express";
 import { fetchChannel } from "../../fetchFeeds";
+import { FetchJob } from "../../jobs/fetchJob";
 import { getSubscribe } from "../../subscribes";
 
 const route = express.Router();
+const job = new FetchJob(5 * 60 * 1000);
 
-route.get("/:id", async (req, resp) => {
+route.get("/interval", (req, resp) => {
+    resp.json({interval: job.interval});
+});
+
+route.post("/", async (req, resp) => {
+    try {
+        await job.check();
+        resp.end();
+    } catch (err) {
+        console.error(`error on feeds.fetch.post /`, err);
+        resp.status(500);
+        resp.end();
+    }
+});
+
+route.post("/:id", async (req, resp) => {
     const id = req.params.id;
     const subscribe = await getSubscribe(id);
     if (subscribe === null) {
@@ -12,12 +29,11 @@ route.get("/:id", async (req, resp) => {
         resp.end();
         return;
     }
-    console.log(subscribe);
     try {
         await fetchChannel(subscribe);
         resp.end();
     } catch (err) {
-        console.error(`error on feeds.fetch.get /${id}`, err);
+        console.error(`error on feeds.fetch.post /${id}`, err);
         resp.status(500);
         resp.end();
     }
