72acf63259c888f51bb0e0fefa11a9970ad6bf2d
20chan <2@0chan.dev>
2020-10-15 17:30:43 +0900

    client item display count button

--- client/app/src/components/channelSection.tsx
+++ client/app/src/components/channelSection.tsx
@@ -4,11 +4,20 @@ import "./channelSection.css";
 
 export const ChannelSection = ({ channel }: { channel: FeedChannel}) => {
     const [displayCount, setDisplayCount] = useState<number>(10);
+
+    const addDisplayCount = (offset: number) => {
+        setDisplayCount(Math.min(Math.max(displayCount + offset, 0), channel.items.length - 1));
+    };
+
     return (
         <section className="channel-section">
             <div className="channel-content">
                 <h1 className="channel-header">
                     <a className="channel-title" href={channel.link}>{channel.name}</a>
+                    <div className="channel-header-corner">
+                        <button className="channel-display-button channel-display-less-button" onClick={() => addDisplayCount(-10)}>-10</button>
+                        <button className="channel-display-button channel-display-more-button" onClick={() => addDisplayCount(+10)}>+10</button>
+                    </div>
                 </h1>
                 <ol className="channel-items">
                 {
@@ -18,6 +27,10 @@ export const ChannelSection = ({ channel }: { channel: FeedChannel}) => {
                     </li>)
                 }
                 </ol>
+                <div className="channel-footer">
+                    <button className="channel-display-button channel-display-less-button" onClick={() => addDisplayCount(-10)}>-10</button>
+                    <button className="channel-display-button channel-display-more-button" onClick={() => addDisplayCount(+10)}>+10</button>
+                </div>
             </div>
         </section>
     );
