8afbb82395507975f8dd9e49dc4d8b4a6c910fe1
20chan <2@0chan.dev>
2020-10-15 11:32:23 +0900

    ezdocker script

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$1" = "--deploy" ]; then
-    CLIENT_DOCKERFILE=Dockerfile docker-compose up --build --force-recreate
-elif [ "$1" = "--develop" ]; then
-    CLIENT_DOCKERFILE=Dockerfile-dev docker-compose up --build --force-recreate
-fi
