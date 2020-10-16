96d82d25524c55468453a9bcd8d618613d3800a9
20chan <2@0chan.dev>
2020-10-16 17:19:17 +0900

    client fetched items are not reversed now

--- client/app/src/dashboard.tsx
+++ client/app/src/dashboard.tsx
@@ -23,7 +23,7 @@ const DashboardPage: React.FunctionComponent<RouteComponentProps> = ({ history }
         const fetchedChannels: IFeedChannel[] = await channelsResp.json();
         const channesWithItems = await Promise.all(fetchedChannels.map(async c => ({
                 ...c,
-                items: (await fetchItem(c._id!)).reverse(),
+                items: (await fetchItem(c._id!)),
         })));
         setChannels(channesWithItems);
     };
